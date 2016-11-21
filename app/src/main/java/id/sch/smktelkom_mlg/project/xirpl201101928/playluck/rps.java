39d0c9967d9481855ffbd8ffae78eb7601e643a2
taufan99 <atalarikt@gmail.com>
2016-11-21 19:52:11 +0700

    Taufan Atalarik 28 ; rps

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl201101928/playluck/rps.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl201101928/playluck/rps.java
@@ -3,11 +3,15 @@ package id.sch.smktelkom_mlg.project.xirpl201101928.playluck;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.Random;
+
+import id.sch.smktelkom_mlg.project.xirpl201101928.playluck.R.drawable;
+import id.sch.smktelkom_mlg.project.xirpl201101928.playluck.R.id;
+import id.sch.smktelkom_mlg.project.xirpl201101928.playluck.R.layout;
 
 
 /**
@@ -26,43 +30,76 @@ public class rps extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.rps);
+        this.setContentView(layout.rps);
 
-        this.iv_cpu = (ImageView) this.findViewById(R.id.iv_cpu);
-        this.iv_me = (ImageView) this.findViewById(R.id.iv_me);
+        this.iv_cpu = (ImageView) findViewById(id.iv_cpu);
+        this.iv_me = (ImageView) findViewById(id.iv_me);
 
-        this.b_rock = (Button) this.findViewById(R.id.b_rock);
-        this.b_paper = (Button) this.findViewById(R.id.b_paper);
-        this.b_scissors = (Button) this.findViewById(R.id.b_scissors);
+        this.b_rock = (Button) findViewById(id.b_rock);
+        this.b_paper = (Button) findViewById(id.b_paper);
+        this.b_scissors = (Button) findViewById(id.b_scissors);
 
         this.r = new Random();
 
-        this.b_rock.setOnClickListener(new OnClickListener() {
+        this.b_rock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                rps.this.myChoice = "rock";
-                rps.this.calculate();
+                myChoice = "rock";
+                calculate();
             }
         });
 
-        this.b_paper.setOnClickListener(new OnClickListener() {
+        this.b_paper.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                rps.this.myChoice = "paper";
-                rps.this.calculate();
+                myChoice = "paper";
+                calculate();
             }
         });
 
-        this.b_scissors.setOnClickListener(new OnClickListener() {
+        this.b_scissors.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                rps.this.myChoice = "scissors";
-                rps.this.calculate();
+                myChoice = "scissors";
+                calculate();
             }
         });
     }
 
-    private void calculate() {
+    public void calculate() {
+        int cpu = this.r.nextInt(3);
+        if (cpu == 0) {
+            this.cpuChoice = "rock";
+            this.iv_cpu.setImageResource(drawable.rock);
+        } else if (cpu == 1) {
+            this.cpuChoice = "paper";
+            this.iv_cpu.setImageResource(drawable.paper);
+        } else if (cpu == 2) {
+            this.cpuChoice = "scissors";
+            this.iv_cpu.setImageResource(drawable.scissors);
+        }
 
+        String result;
+        if (this.myChoice.equals("rock") && this.cpuChoice.equals("paper")) {
+            result = "you lose";
+        } else if (this.myChoice.equals("rock") && this.cpuChoice.equals("scissors")) {
+            result = "you win";
+        } else if (this.myChoice.equals("paper") && this.cpuChoice.equals("rock")) {
+            result = "you win";
+        } else if (this.myChoice.equals("paper") && this.cpuChoice.equals("scissors")) {
+            result = "you lose";
+        } else if (this.myChoice.equals("scissors") && this.cpuChoice.equals("paper")) {
+            result = "you win";
+        } else if (this.myChoice.equals("scissors") && this.cpuChoice.equals("rock")) {
+            result = "you lose";
+        } else if (this.myChoice.equals("rock") && this.cpuChoice.equals("rock")) {
+            result = "draw";
+        } else if (this.myChoice.equals("paper") && this.cpuChoice.equals("paper")) {
+            result = "draw";
+        } else if (this.myChoice.equals("scissors") && this.cpuChoice.equals("scissors")) {
+            result = "draw";
+        }
+
+        Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
     }
 }
