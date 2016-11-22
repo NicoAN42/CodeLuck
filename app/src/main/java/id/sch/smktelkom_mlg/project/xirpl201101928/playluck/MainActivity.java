d3bc778e8ecd44bb8603cd007eeb3b38e50f27c0
Ilham Izzul <ilhamizzul@gmail.com>
2016-11-22 20:36:49 +0700

    Ilham Izzul Hadyan/10/XI RPL 2 Link Java Class 1

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl201101928/playluck/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl201101928/playluck/MainActivity.java
@@ -1,21 +1,48 @@
 package id.sch.smktelkom_mlg.project.xirpl201101928.playluck;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
+    ImageButton dice;
+    ImageButton rps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.addListenerOnButton();
+
+
+    }
+
+    public void addListenerOnButton() {
+        this.dice = (ImageButton) this.findViewById(R.id.dice);
+
+        this.dice.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Toast.makeText(MainActivity.this,
+                        "random DICE!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        this.rps = (ImageButton) this.findViewById(R.id.rps);
+
+        this.rps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "Rock Paper Scissor", Toast.LENGTH_SHORT).show();
+            }
+        });
 }}
