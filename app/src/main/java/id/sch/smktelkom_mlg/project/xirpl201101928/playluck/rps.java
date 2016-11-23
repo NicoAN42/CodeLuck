63f61e582da13793c43a0fa7211d74fb94be0deb
Aisyatush Shofiah <aisyatushshofiah@gmail.com>
2016-11-23 13:23:56 +0700

    Aisyatush Shofiah 01 ; fixing java rps

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl201101928/playluck/rps.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl201101928/playluck/rps.java
@@ -3,15 +3,13 @@ package id.sch.smktelkom_mlg.project.xirpl201101928.playluck;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.util.Random;
 
-import id.sch.smktelkom_mlg.project.xirpl201101928.playluck.R.drawable;
-import id.sch.smktelkom_mlg.project.xirpl201101928.playluck.R.id;
+import id.sch.smktelkom_mlg.project.xirpl201101928.playluck.R.layout;
 
 public class rps extends AppCompatActivity {
 
@@ -25,40 +23,40 @@ public class rps extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rps);
+        this.setContentView(layout.activity_rps);
 
-        this.iv_cpu = (ImageView) this.findViewById(id.iv_cpu);
-        this.iv_me = (ImageView) this.findViewById(id.iv_me);
+        this.iv_cpu = (ImageView) this.findViewById(R.id.iv_cpu);
+        this.iv_me = (ImageView) this.findViewById(R.id.iv_me);
 
-        this.b_rock = (Button) this.findViewById(id.b_rock);
-        this.b_paper = (Button) this.findViewById(id.b_paper);
-        this.b_scissors = (Button) this.findViewById(id.b_scissors);
+        this.b_rock = (Button) this.findViewById(R.id.b_rock);
+        this.b_paper = (Button) this.findViewById(R.id.b_paper);
+        this.b_scissors = (Button) this.findViewById(R.id.b_scissors);
 
         this.r = new Random();
 
-        this.b_rock.setOnClickListener(new OnClickListener() {
+        this.b_rock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 rps.this.myChoice = "rock";
-                rps.this.iv_me.setImageResource(drawable.rock);
+                rps.this.iv_me.setImageResource(R.drawable.rock);
                 rps.this.calculate();
             }
         });
 
-        this.b_paper.setOnClickListener(new OnClickListener() {
+        this.b_paper.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 rps.this.myChoice = "paper";
-                rps.this.iv_me.setImageResource(drawable.paper);
+                rps.this.iv_me.setImageResource(R.drawable.paper);
                 rps.this.calculate();
             }
         });
 
-        this.b_scissors.setOnClickListener(new OnClickListener() {
+        this.b_scissors.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 rps.this.myChoice = "scissors";
-                rps.this.iv_me.setImageResource(drawable.scissors);
+                rps.this.iv_me.setImageResource(R.drawable.scissors);
                 rps.this.calculate();
             }
         });
@@ -68,13 +66,13 @@ public class rps extends AppCompatActivity {
         int cpu = this.r.nextInt(3);
         if (cpu == 0) {
             this.cpuChoice = "rock";
-            this.iv_cpu.setImageResource(drawable.rock);
+            this.iv_cpu.setImageResource(R.drawable.rock);
         } else if (cpu == 1) {
             this.cpuChoice = "paper";
-            this.iv_cpu.setImageResource(drawable.paper);
+            this.iv_cpu.setImageResource(R.drawable.paper);
         } else if (cpu == 2) {
             this.cpuChoice = "scissors";
-            this.iv_cpu.setImageResource(drawable.scissors);
+            this.iv_cpu.setImageResource(R.drawable.scissors);
         }
 
         if (this.myChoice.equals("rock") && this.cpuChoice.equals("paper")) {
